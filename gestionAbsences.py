dd36bb5ef082298a5537fde8fa92c4924db2baf4
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2011-12-15 17:45:44 +0100

    L'ui s'affiche correctement

--- gestionAbsences.py
+++ gestionAbsences.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from gestionAbsencesUI import Ui_gestionAbsences
 
+class GestionAbsences(QTabWidget):
+	def __init__(self, parent=None):
+		super(GestionAbsences, self).__init__(parent)
+		self.createWidgets()
+
+	def createWidgets(self):
+		self.ui = Ui_gestionAbsences()
+		self.ui.setupUi(self)
+
 if __name__ == "__main__":
-	print "that's it !"
+	app = QApplication(sys.argv)
+	ui = GestionAbsences()
+	ui.show()
+	sys.exit(app.exec_())
