c28090cb2dc5c9483e290f3a68de201976d4e60b
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2012-01-12 18:27:14 +0100

    [UI]La fenêtre principale est une QMainWindow

--- gem.py
+++ gem.py
@@ -4,28 +4,27 @@
 import sys
 from PyQt4.QtCore import QLocale, QTranslator, QString, QLibraryInfo, QRect
 
-from PyQt4.QtGui import QApplication, QTabWidget, QDesktopWidget
+from PyQt4.QtGui import QApplication, QMainWindow
 
 from PyQt4.QtSql import QSqlDatabase
 
-from gemUI import Ui_gestionAbsences
+from gemUI import Ui_gem
 from mail import MailUI
 from absence import AbsenceUI
 from intervenant import IntervenantUI
 from config import ConfigUI, Config
 
 
-class GestionAbsences(QTabWidget):
+class GestionAbsences(QMainWindow):
     def __init__(self, parent=None):
         super(GestionAbsences, self).__init__(parent)
-        screenIndex = 0
-        monEcran = QDesktopWidget()
-        monEcran.screenGeometry(screenIndex).width()
-        monEcran.screenGeometry(screenIndex).height()
         self.createWidgets()
         self.conf = Config.getInstance()
         self.mailTab.miseAJour()
+        self.__connectSlots()
 
+    def __connectSlots(self):
+        u"""Connecte les signaux de rafraîchissement du contenu des onglets"""
         self.mailTab.majBdd.connect(self.absenceTab.miseAJour)
         self.absenceTab.majBdd.connect(self.mailTab.miseAJour)
         self.intervenantTab.majBdd.connect(self.mailTab.miseAJour)
@@ -33,18 +32,20 @@ class GestionAbsences(QTabWidget):
         self.configTab.majDuree.connect(self.mailTab.miseAJour)
 
     def createWidgets(self):
-        self.__ui = Ui_gestionAbsences()
+        self.__ui = Ui_gem()
         self.__ui.setupUi(self)
+        self.__ajouteOnglets(self.__ui.tabWidget)
 
-        # Ajout des onglets
+    def __ajouteOnglets(self, tabWidget):
+        u"""Construit et attache les onglets au tab widget"""
         self.mailTab = MailUI(self)
-        self.addTab(self.mailTab, "Emails")
+        tabWidget.addTab(self.mailTab, "Emails")
         self.absenceTab = AbsenceUI(self)
-        self.addTab(self.absenceTab, "Absences")
+        tabWidget.addTab(self.absenceTab, "Absences")
         self.intervenantTab = IntervenantUI(self)
-        self.addTab(self.intervenantTab, "Intervenants")
+        tabWidget.addTab(self.intervenantTab, "Intervenants")
         self.configTab = ConfigUI(self)
-        self.addTab(self.configTab, "Configuration")
+        tabWidget.addTab(self.configTab, "Configuration")
 
 
 if __name__ == "__main__":
